eba2e3e04006afd3c632325d93190599dc3a714f
Pedro Nascimento Martins <pedronm0627@gmail.com>
2025-08-01 22:17:54 -0300

    VariaveisDecimal

---
+++ 02_Variaveis/DeclaracaoDecimal.cs
@@ -0,0 +1,10 @@
+using System;
+class DeclaracaoVariaveldecimal 
+	{
+		static void Main(string)] agrs)
+		{
+			//decimal myMoneyError = 300.0;
+			decimal myMoneyCorrect = 300.0m;
+			Console.WriteLine (myMoneyCorrect);
+		}
+	}
